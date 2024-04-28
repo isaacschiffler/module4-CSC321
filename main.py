62b754b98b5ad39efb4216ebb22160418ec23dcd
Isaac Schiffler <ischiffl@calpoly.edu>
2024-04-28 16:57:04 -0700

    task1c

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ from Crypto.Util.Padding import unpad
 from urllib.parse import quote
 from Crypto.Util import number
 import os
+import random
+import time
+
 
 def task1_a(input):
     print("Input text: " + input)
@@ -16,6 +19,7 @@ def task1_a(input):
     hash = hashlib.sha256(input_bytes).hexdigest()
     print("Hex Digest of input: 0x" + hash)
     return hash
+
 
 def task1_b(str1, str2):
     print("Hash " + str1 + " vs. " + str2)
@@ -28,6 +32,50 @@ def task1_b(str1, str2):
     return
 
 
+def task1_c(bits):
+    # get start time
+    start_time = time.time()
+    # create the message dictionary
+    messages = message_gen(bits)
+    # generate hash for each message in the dictionary
+    for key in messages.keys():
+        # assign hash to key in dictionary
+        messages[key] = c_helper(key, bits)
+
+    for key1 in messages.keys():
+        for key2 in messages.keys():
+            if messages[key1] == messages[key2] and key1 != key2:
+                # get collision time
+                end_time = time.time()
+                print("m0: " + key1)
+                print("m1: " + key2)
+                print("hash0: " + str(messages[key1]))
+                print("hash1: " + str(messages[key2]))
+                print("COLLISION FOUND!")
+                print("Elapsed time: " + str(end_time - start_time) + " seconds\n")
+                return
+
+    end_time = time.time()
+    print("No collision found in " + str(end_time - start_time) + " seconds")
+    return
+
+
+def c_helper(input, size: int):
+    # function to get the truncated binary hash given the size requested
+    input_bytes = input.encode('utf-8')
+    hash_digest = hashlib.sha256(input_bytes).digest()
+    binary_hash = ''.join(format(byte, '08b') for byte in hash_digest) # format binary
+    hash_trunc = binary_hash[:size] # get up to [size] bits
+    return hash_trunc
+
+
+def message_gen(bits):
+    dict = {}
+    for i in range(0, (2 ** bits) // 8):
+        dict[str(i)] = 0
+    return dict
+
+
 
 if __name__ == '__main__':
     print("---------------------------- Task 1 ----------------------------")
@@ -38,5 +86,8 @@ if __name__ == '__main__':
     task1_b("a", "b")
     task1_b("hello", "hellp")
     print("\n-- c --")
-
+    for i in range(8, 51, 2):
+        print(str(i) + " bit hash truncation...")
+        task1_c(i)
+    print("\nDone with task 1c!")
 
