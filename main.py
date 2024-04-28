ee14c40d7c999d2674806965fe66138610d8ee73
Isaac Schiffler <ischiffl@calpoly.edu>
2024-04-28 15:37:43 -0700

    task1b

--- main.py
+++ main.py
@@ -17,8 +17,14 @@ def task1_a(input):
     print("Hex Digest of input: 0x" + hash)
     return hash
 
-def task1_b():
-
+def task1_b(str1, str2):
+    print("Hash " + str1 + " vs. " + str2)
+    input_bytes = str1.encode('utf-8')
+    hash1 = hashlib.sha256(input_bytes).hexdigest()
+    input_bytes = str2.encode('utf-8')
+    hash2 = hashlib.sha256(input_bytes).hexdigest()
+    print("Hash1: 0x" + hash1)
+    print("Hash2: 0x" + hash2)
     return
 
 
@@ -28,4 +34,9 @@ if __name__ == '__main__':
     print("-- a --")
     task1_a("Hello there")
     print("\n-- b --")
+    task1_b("1", "2")
+    task1_b("a", "b")
+    task1_b("hello", "hellp")
+    print("\n-- c --")
+
 
